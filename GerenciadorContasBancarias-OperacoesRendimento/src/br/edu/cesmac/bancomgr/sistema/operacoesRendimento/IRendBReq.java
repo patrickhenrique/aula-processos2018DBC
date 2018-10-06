320f67ae5a8f80a0159cfc5ecb6266a261cf160b
Victor Mota <victor.mota892@gmail.com>
2018-10-06 15:41:14 -0300

    Update IRendBReq.java

--- GerenciadorContasBancarias-OperacoesRendimento/src/br/edu/cesmac/bancomgr/sistema/operacoesRendimento/IRendBReq.java
+++ GerenciadorContasBancarias-OperacoesRendimento/src/br/edu/cesmac/bancomgr/sistema/operacoesRendimento/IRendBReq.java
@@ -1,5 +1,13 @@
 package br.edu.cesmac.bancomgr.sistema.operacoesRendimento;
 
+import java.util.List;
+
+import br.edu.cesmac.bancomgr.sharedmodel.Conta;
+
 public interface IRendBReq {
+
+	List<Conta> obterContas();
+
+	void alterarConta(int numero, Conta conta);
 
 }
