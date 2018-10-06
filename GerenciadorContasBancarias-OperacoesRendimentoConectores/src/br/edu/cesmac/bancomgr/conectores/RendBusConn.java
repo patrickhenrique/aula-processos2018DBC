8e151254b1c354520a3474eb1d1344bdcc8d008a
Victor Mota <victor.mota892@gmail.com>
2018-10-06 15:43:08 -0300

    Update RendBusConn.java

--- GerenciadorContasBancarias-OperacoesRendimentoConectores/src/br/edu/cesmac/bancomgr/conectores/RendBusConn.java
+++ GerenciadorContasBancarias-OperacoesRendimentoConectores/src/br/edu/cesmac/bancomgr/conectores/RendBusConn.java
@@ -1,13 +1,30 @@
 package br.edu.cesmac.bancomgr.conectores;
 
+import java.util.List;
+
 import br.edu.cesmac.bancomgr.business.BusinessController;
 import br.edu.cesmac.bancomgr.business.INegocio;
+import br.edu.cesmac.bancomgr.sharedmodel.Conta;
 import br.edu.cesmac.bancomgr.sistema.operacoesRendimento.IRendBReq;
 
 public class RendBusConn implements IRendBReq{
 	private INegocio business;
 	
-	public RendBusConn() {
+	public RendBusConn() { 
 		this.business = new BusinessController();
+	}
+
+
+	@Override
+	public List<Conta> obterContas() {
+		// TODO Auto-generated method stub
+		
+		return this.business.obterContas();
+	}
+
+	@Override
+	public void alterarConta(int numero, Conta conta) {
+		// TODO Auto-generated method stub
+		this.business.alterarConta(numero, conta);
 	}
 }
